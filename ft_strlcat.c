2ff30b2414d5ec9c127f53796537408b80538e44
Stepan Shabalinov <shabalinov2341234@gmail.com>
2024-11-16 18:31:21 +0300

    update naming

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -12,21 +12,21 @@
 
 #include "libft.h"
 
-size_t	ft_strlcat(char *dest, const char *src, size_t size)
+size_t	ft_strlcat(char *dst, const char *src, size_t size)
 {
 	size_t	d;
 	size_t	s;
 
 	d = 0;
-	while (dest[d] && d < size)
+	while (dst[d] && d < size)
 		d++;
 	s = 0;
 	while (src[s] && (d + s + 1) < size)
 	{
-		dest[d + s] = src[s];
+		dst[d + s] = src[s];
 		s++;
 	}
 	if (d != size)
-		dest[d + s] = '\0';
+		dst[d + s] = '\0';
 	return (d + ft_strlen(src));
 }
